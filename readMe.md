21e56d4a5de6202b17941dab01497a83cbb63992
i848920 <shams.abubakar.azad@sap.com>
2019-08-18 13:56:58 -0400

    Added unit test for APIError Class

--- readMe.md
+++ readMe.md
@@ -1,1 +1,1 @@
-To run this program locally add the provide geoNames library in internalLib to your local https://www.mkyong.com/maven/how-to-include-library-manully-into-maven-local-repository/
+How to run
--- src/test/java/com/coveo/citiesfinder/models/response/ApiErrorTest.java
+++ src/test/java/com/coveo/citiesfinder/models/response/ApiErrorTest.java
@@ -1,9 +1,40 @@
 package com.coveo.citiesfinder.models.response;
 
+import com.google.common.testing.EqualsTester;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+
 public class ApiErrorTest {
 
+    @Test
     public void testEqualsAndHashCode() {
 
+        ApiError apiError = createApiError();
+        apiError.setTimestamp(null);
+        
+        ApiError apiError1 = createApiError();
+        apiError1.setTimestamp(null);
 
+        ApiError apiError2 = createApiError();
+        apiError2.setStatus(HttpStatus.BAD_REQUEST);
+        apiError2.setTimestamp(null);
+
+        ApiError apiError3 = createApiError();
+        apiError3.setStatus(HttpStatus.BAD_REQUEST);
+        apiError3.setTimestamp(null);
+
+        new EqualsTester()
+                .addEqualityGroup(apiError, apiError1)
+                .addEqualityGroup(apiError2)
+                .testEquals();
+
+        Assert.assertNotEquals(apiError.hashCode(), apiError2.hashCode());
+        Assert.assertEquals(apiError2.hashCode(), apiError3.hashCode());
+    }
+
+    private ApiError createApiError() {
+
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
